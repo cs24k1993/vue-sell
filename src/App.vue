27e08b37a63fce55ca1fd626f8fb950b9153a919
cs24k1993 <1344529446@qq.com>
2020-04-03 22:11:20 +0800

    4/3 Update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
-    <div class="header">
-      <v-header :seller="seller"></v-header>
+    <v-header></v-header>
+    <!--border-1px不是函数吗？怎么当类名使用了呢？-->
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
     </div>
-    <div class="tab">
-      I am tab!
-    </div>
-    <div class="content">
-      I am content!
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
   import VHeader from './components/v-header/v-header'
+
   export default {
     components: {
       VHeader
@@ -21,6 +27,25 @@
   }
 </script>
 
-<style lang="stylus">
-
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+  #app
+    .tab
+      display: flex
+      width: 100%
+      height: 40px
+      line-height: 40px
+      border-1px(rgba(7, 17, 27, 0.1))
+      .tab-item
+        flex: 1
+        text-align: center
+        // router-link在网页中被渲染成a标签，设置css时要用a标签
+        & > a
+          display: block
+          text-decoration: none
+          font-size: 14px
+          color: rgb(77, 85, 93)
+          // 相当于a.active，为什么可以这么用？
+          &.active
+            color: rgb(240, 20, 20)
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
 import Vue from 'vue'
-import './cube-ui'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import App from './App'
+import goods from 'components/goods/goods'
+import ratings from 'components/ratings/ratings'
+import seller from 'components/seller/seller'
 
-Vue.config.productionTip = false
+import 'common/stylus/index.styl'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/goods', component: goods },
+  { path: '/ratings', component: ratings },
+  { path: '/seller', component: seller },
+  { path: '/', redirect: '/goods' }
+]
+const router = new VueRouter({
+  routes,
+  linkActiveClass: 'active'
+})
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
